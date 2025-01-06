c2ab34d398fdf4ea2c2092b54e533497fb16c1ea
ashikurrahman1052@gmail.com <ashikurrahman1052@gmail.com>
2025-01-06 16:24:23 +0600

    Additional practice on list

--- second.py
+++ second.py
@@ -9,13 +9,27 @@
 
 
 #PythonList
-mylist = ["apple","11","33.00"]
-print(len(mylist), type(mylist))
-thislist = list(("apple","55","243hf43"))
-print(thislist)
-
-wishList = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-wishList[1:3] = ["blackcurrant", ""]
-print(wishList)
-wishList.insert(2,"Latte")
-print(wishList)
+# mylist = ["apple","11","33.00"]
+# print(len(mylist), type(mylist))
+# thislist = list(("apple","55","243hf43"))
+# print(thislist)
+#
+# wishList = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# wishList[1:3] = ["blackcurrant", ""]
+# print(wishList)
+# wishList.insert(2,"Latte")
+# print(wishList)
+# # wishList.append("strawberry")
+# # print(wishList)
+# # wishList.extend(thislist)
+# # print(wishList)
+#
+# mylist = ['apple', 'banana', 'cherry']
+# mylist.insert(0, 'orange')
+# print(mylist)
+# mylist.pop(1)
+# print(mylist)
+# mylist.pop()
+# print(mylist)
+# mylist.clear()
+# print(mylist)
