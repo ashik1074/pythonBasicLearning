7cc0cdbae53cb03b710454dfd5a9f0b5d154e350
ashikurrahman1052@gmail.com <ashikurrahman1052@gmail.com>
2025-01-28 12:23:13 +0600

    Learning Functions and Lambda basics

--- functionBasics.py
+++ functionBasics.py
@@ -4,3 +4,18 @@ def myFunction(food):
 
 fruits = ["Banana","Apples", "Cherry"]
 print(myFunction(fruits))
+
+# def demoFunction(a):
+#     return lambda a:a+n
+#
+# adder = demoFunction(5)
+# print(demoFunction(2))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
